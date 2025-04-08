078055765eb6464512905bdcdf6878f32f6e02b6
chimin14 <aminajusic61@gmail.com>
2025-04-08 14:01:48 +0200

    Project shell
    Project shell created

---
+++ job-drop/src/pages/test2.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+    return (
+      <div>
+        <h1>WelcomePage</h1>
+        <p><a href="test2">Otvori me</a>This is a new page in your Next.js app.</p>
+        
+      </div>
+    );
+  }
