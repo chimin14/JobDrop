cf6b3c3a493dcf6aaae6fcffcb6594f6536bea12
DejanSucur <sucur.dejan2@gmail.com>
2025-05-24 01:41:01 +0200

    Removed unnecessary files

--- job-drop/src/pages/test2.tsx
+++
@@ -1,9 +0,0 @@
-export default function TestPage() {
-    return (
-      <div>
-        <h1>WelcomePage</h1>
-        <p>This is a new page in your Next.js app.</p>
-        
-      </div>
-    );
-  }
